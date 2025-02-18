cc3c823e2b214ace0b09ca6db3d2618ec0c86a14
Andy Mauragis <andrew@mauragis.com>
2025-02-18 14:08:02 -0500

    extern C aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/ping/ping.h
+++ src/ping/ping.h
@@ -1,19 +1,16 @@
 #ifndef LWIP_PING_H
 #define LWIP_PING_H
 
-#include "lwip/ip_addr.h"
-
-/**
- * PING_USE_SOCKETS: Set to 1 to use sockets, otherwise the raw api is used
- */
-#ifndef PING_USE_SOCKETS
-#define PING_USE_SOCKETS    LWIP_SOCKET
+#ifdef __cplusplus
+extern "C" {
 #endif
+
+#include "lwip/ip_addr.h"
 
 void ping_init(const ip_addr_t* ping_addr);
 
-#if !PING_USE_SOCKETS
-void ping_send_now(void);
-#endif /* !PING_USE_SOCKETS */
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* LWIP_PING_H */
